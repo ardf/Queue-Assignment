1e909e823bec8fe61bb00af6a9eafb9ec01e6448
Aakif Ashar Khan <akifkhan892@gmail.com>
2022-10-17 15:06:24 +0530

    Simple Queue and Fixed Sized Queue

---
+++ Queue2.py
@@ -0,0 +1,57 @@
+class Queue:
+    def __init__(self):
+        self.data = []
+        self.count = 0
+        self.size = 10
+
+    def is_Full(self):
+        return self.count == self.size
+
+    def is_Empty(self):
+        return self.count == 0
+
+    def enQueue(self, element):
+        if self.is_Full():
+            print("Queue is Full")
+            return
+        else:
+            self.data.append(element)
+            self.count += 1
+            print("Inserted Successfully")
+            return
+
+    def deQueue(self):
+        if self.is_Empty():
+            print("Queue is Empty")
+            return
+        else:
+            print("Popped out Successfully")
+            self.count -= 1
+            return self.data.pop(0)
+
+    def peek(self):
+        return self.data[-1]
+
+    def print_queue(self):
+        print("printingnining")
+        for i in self.data:
+            print(i)
+        return
+
+
+q1 = Queue()
+print("Queue full: ", q1.is_Full())
+print("Queue empty: ", q1.is_Empty())
+q1.enQueue(10)
+q1.enQueue(20)
+q1.enQueue(30)
+q1.enQueue(40)
+q1.enQueue(60)
+# print(q1.peek())
+print(q1.deQueue())
+# print(q1.peek())
+print(q1.deQueue())
+# print(q1.peek())
+print(q1.deQueue())
+# print(q1.peek())
+print(q1.print_queue())
